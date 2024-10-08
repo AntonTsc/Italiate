2bbde72677caf1376d501b0b06fb390476e6365b
Anton Tschanz <a_tschanzshu@fpzornotza.com>
2024-10-08 23:35:55 +0200

    Formulario cambios
    Se ha añadido js para verificar que los campos esten rellenos a la hora de enviar los datos, falta terminar la página de formulario-mensaje.html

--- resources/js/formulario.js
+++ resources/js/formulario.js
@@ -1,6 +1,24 @@
 //Mostrar los datos del formulario por pantalla y poner un mensaje de como "gracias por contactarnos" y un boton para volver a la pagina de inicio, y si puede ser que cierre la ventana emergente.
+
+document.getElementById("botonEnviar").addEventListener('click', function(){
+    //Recoge los datos del formulario
+    const nombre = document.getElementById('nombre').value;
+    const email = document.getElementById('email').value;
+    const asunto = document.getElementById('asunto').value;
+    const descripcion = document.getElementById('descripcion').value;
+
+    //Verifica si los campos están vacíos
+    if(nombre === '' || email === '' | asunto === '' | descripcion === ''){
+        alert('Por favor, rellene todos los campos.');
+    } else {
+        //Si están rellenos, se ejecuta la función
+        openWindow();
+    }
+});
+
+//Función que abre la ventana al enviar el formulario.
 function openWindow(){
-    let ventana = window.open("destinos.html", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400");
+    let ventana = window.open("formulario-mensaje.html", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400");
 }
 
 
