cbcc16ea2c14cf10b3596c4e7a173e866ec02ab5
Anton Tschanz <a_tschanzshu@fpzornotza.com>
2024-10-13 16:25:07 +0200

    Cambios con todo lo relacionado al formulario
     -Correción filtro teléfono.
    - Limite de caracteres para cada input agregados, y css para que el texto se adapte automaticamente a la tabla.
    - Mini limpieza de código
    - Comentarios agregados

--- resources/js/formulario.js
+++ resources/js/formulario.js
@@ -1,41 +1,46 @@
-//Hacer que los inputs tengan un limite de caracteres, y que por ejemplo en el textarea, que tiene un max de 300 caracteres, te ponga mientras vayas escribiendo cuantos caracteres pusiste, y si superas los 300 salga en rojo
-console.log(document.getElementById('nombre').value);
+// Se añade un EventListener al botón para que, al hacerle click, ejecute una función
 document.getElementById("botonEnviar").addEventListener('click', function(){
-    //Recoge los datos del formulario
+    // Recoge los datos de los campos del formulario
     const nombre = document.getElementById('nombre');
     const email = document.getElementById('email');
     const telefono = document.getElementById('telefono');
     const asunto = document.getElementById('asunto');
     const descripcion = document.getElementById('descripcion');
-    //Se meten todos los datos en un array
+
+    // Se meten todos los datos recogidos en un array
     const datos = [nombre, email, telefono, asunto, descripcion];
     
+    // Se crea un enlace con los valores de los campos del formulario
     let link = "formulario-mensaje.html?nombre=" + datos[0].value + "&email=" + datos[1].value + "&telefono=" + datos[2].value + "&asunto=" + datos[3].value + "&descripcion=" + datos[4].value;
 
-    //Verifica si los campos están vacíos
+    // Verificación para ver si los campos están vacíos
     let correcto = 0;
+    // Recorre cada campo del array para verificar si está vacío
     datos.forEach(dato => {
-        
+        // Si el campo está vacío, el borde se pone rojo
         if(dato.value === ''){
             dato.style.borderColor = "red";
         } else {
+            // Si el campo tiene contenido, el borde se pone verde
             dato.style.borderColor = "green";
             correcto++;
         }
     })
-    //Verificar que los datos (email y telefono) cumplan los requisitos (regex)
+    // Validación del formato del correo electrónico utilizando regex
     const validarEmail = (email) => {
         return String(email).toLowerCase().match(
-            /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+            /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/ // Patrón para emails
         );
     };
 
+    // Validación del formato del número de teléfono utilizando regex
     const validarTel = (telefono) => {
         return String(telefono).match(
-            /^\+?[1-9]\d{1,14}$/
+            /^\+?[1-9]\d{0,2}(?:\s?\d{2,3}){3,5}$/ // Patrón para números de teléfonos
         );
     };
 
+    // Si el email o el teléfono no cumplen con el formato correcto, pone el borde en rojo y marca el formulario como incorrecto
     if (validarEmail(email.value) == null){
         email.style.borderColor = 'red';
         correcto = 0;
@@ -44,15 +49,17 @@ document.getElementById("botonEnviar").addEventListener('click', function(){
         correcto = 0;
     }
 
+    // Si todos los campos son válidos, se abre una nueva ventana
     if (correcto == 5){
         openWindow(link);
-    }else {
+    } else {
+        // Si no se validaron todos los campos, se muestra una alerta
         alert('Por favor, rellene todos los campos.');
 
     }
 });
 
-//Función que abre la ventana al enviar el formulario.
+// Función que abre una nueva ventana con el enlace creado en el formulario
 function openWindow(link){
     let ventana = window.open(link, "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=650,height=600");
 }
